5d03a974a4862113b3cfce992d939992a91e6be0
sei218 <mseisei0218@gmail.com>
2025-06-18 16:58:25 +0900

    ex7

---
+++ ex7.py
@@ -0,0 +1,12 @@
+from ultralytics import YOLO
+
+model=YOLO("othello.pt")
+
+results=model.predict("ex4.jpg", conf=0.99)
+labels=results[0].boxes.cls.tolist()
+#カウント
+count_white = labels.count(0)
+count_black = labels.count(1)
+
+print(f"白の石:{count_white}個")
+print(f"黒の石:{count_black}個")
